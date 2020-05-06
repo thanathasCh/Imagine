a5427a8adf51446cc26e6bcd4538184480475cae
Thanathas Chawengvorakul <tamasama077@gmail.com>
2020-05-06 14:42:46 +0700

    Fix PR

--- main.py
+++ main.py
@@ -90,7 +90,6 @@ def addEventSubmit():
         name = request.form['eventname']
         description = request.form['description']
         date = request.form['date']
-        print(date)
         poster = request.files['posterImage']
         files = request.files.getlist('eventImages')
         secondFiles = request.files.getlist('secondEventImages')
